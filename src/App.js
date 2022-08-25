c9527ee9727501ae96578086415d23c8ef5c1902
Konstantin Shambazov <k.shambazov@gmail.com>
2022-08-25 15:01:18 +0900

    feat: add get exchange rates functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import {Block} from './Block'
+import { useEffect, useState } from 'react';
+import { Block } from './Block';
+import axios from 'axios';
 
 function App() {
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('https://cdn.cur.su/api/latest.json')
+      .then((res) => res.data)
+      .then((json) => setRates(json.rates))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <Block value={0} currency="JPY" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="JPY"
+        onChangeCurr={(curr) => console.log(curr)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
