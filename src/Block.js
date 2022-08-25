7b7c85293e1de621571f30574b74c4d661a0fd60
Konstantin Shambazov <k.shambazov@gmail.com>
2022-08-25 14:35:39 +0900

    feat: add svg arrow and input currency input form

--- src/Block.js
+++ src/Block.js
@@ -5,8 +5,32 @@ export const Block = ({ value, currency, onChangeVal, onChangeCurr }) => (
   <div className="block">
     <ul className="currencies">
       {defaultCurrencies.map((curr) => (
-        <li onClick={() => onChangeCurr(curr)} key={curr}>{curr}</li>
+        <li onClick={() => onChangeCurr(curr)} key={curr}>
+          {curr}
+        </li>
       ))}
+      <li>
+        <svg
+          width="70"
+          height="55"
+          viewBox="-2.5 -5 75 60"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,0 l35,50 l35,-50"
+            fill="none"
+            stroke="black"
+            stroke-linecap="round"
+            stroke-width="5"
+          />
+        </svg>
+      </li>
     </ul>
+    <input
+      onChange={(e) => onChangeVal(e.target.value)}
+      value={value}
+      type="number"
+      placeholder={0}
+    />
   </div>
 );
